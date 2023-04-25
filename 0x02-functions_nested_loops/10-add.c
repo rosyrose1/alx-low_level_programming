307aa861e190edbb11717eb4c259fe5de9255b42
rosyrose1 <rosinaantwi96@gmail.com>
2023-04-25 07:11:39 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - print out addition of two numbers
+ *@x: first value
+ *@y: second value
+ * Return: sum of two numbers
+ */
+
+int add(int x, int y)
+{
+int i;
+i = x + y;
+return (i);
+}
+
