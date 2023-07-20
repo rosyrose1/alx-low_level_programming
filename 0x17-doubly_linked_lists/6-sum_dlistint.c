92e02d5383e01ee638bfeb0ef7ccc12cf8d80570
rosyrose1 <128856014+rosyrose1@users.noreply.github.com>
2023-07-20 10:28:53 +0000

    Create 6-sum_dlistint.c

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sum all items in a doubly-linked list
+ * @head: pointer to the beginning of the list
+ *
+ * rosy's project
+ * Return: The sum of all items in the list
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	for ( ; head; head = head->next)
+		sum += head->n;
+
+	return (sum);
+}
