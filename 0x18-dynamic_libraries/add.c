cda40f8ea6747f9401c9a39a00fb0d706c9c0ed3
rosyrose1 <128856014+rosyrose1@users.noreply.github.com>
2023-07-24 15:50:45 +0000

    Update add.c

--- 0x18-dynamic_libraries/add.c
+++ 0x18-dynamic_libraries/add.c
@@ -1,29 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 /**
- * main - Printing the sum of positive numbers,followed by a new line.
- * @argc: The number of arguments passed to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: If one of the numbers contains symbols that are non-digits - 1.
- *         Otherwise - 0.
- */
-int main(int argc, char *argv[])
+* add - adds two given integers
+*
+* @a: first integer
+* @b: second integer
+*
+* Return: sum of the two integers
+*/
+int add(int a, int b)
 {
-	int num, digit, sum = 0;
-
-	for (num = 1; num < argc; num++)
-	{
-		for (digit = 0; argv[num][digit]; digit++)
-		{
-			if (argv[num][digit] < '0' || argv[num][digit] > '9')
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		sum += atoi(argv[num]);
-	}
-	printf("%d\n", sum);
-	return (0);
+int sum;
+sum = a + b;
+return (sum);
 }
