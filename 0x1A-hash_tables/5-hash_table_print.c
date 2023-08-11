4e00f337e2f269bf08d8a3e368727f15ae2aba5e
rosyrose1 <rosinaantwi96@gmail.com>
2023-08-11 02:30:30 -0700

    a function that prints a hash table.

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,64 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+/**
+ * hash_table_print_sep - Output a delimiter b\n elements within a hash table.
+ *
+ */
+void hash_table_print_sep(void)
+{
+	fputs(", ", stdout);
+}
+
+/**
+ * hash_chain_print - Outputs the elements in a singly-linked list
+ * @head: pointer to the singly-linked list
+ */
+void hash_chain_print(const hash_node_t *head)
+{
+	for (;;)
+	{
+		if (head->value)
+			printf("'%s': '%s'", head->key, head->value);
+		else
+			printf("'%s': %s", head->key, head->value);
+
+		head = head->next;
+
+		if (head)
+			hash_table_print_sep();
+		else
+			return;
+	}
+}
+
+/**
+ * hash_table_print - outputs the elements in a hash table
+ * @ht: pointer to the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	void (*print_sep)() = NULL;
+	hash_node_t **array = NULL;
+	unsigned long int index = 0;
+	unsigned long int size = 0;
+
+	if (ht)
+	{
+		putchar('{');
+		for (array = ht->array, size = ht->size; index < size; ++index)
+		{
+			if (array[index])
+			{
+				if (print_sep)
+					print_sep();
+				else
+					print_sep = hash_table_print_sep;
+
+				hash_chain_print(array[index]);
+			}
+		}
+		puts("}");
+	}
+}
+
